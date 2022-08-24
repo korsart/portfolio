bf854be61c67d5d21d280bc5394958aa12a49264
korsart <104280131+korsart@users.noreply.github.com>
2022-08-24 12:21:10 +0300

    Update urls.py

--- webjox_backend_promo/urls.py
+++ webjox_backend_promo/urls.py
@@ -3,4 +3,13 @@ from .views import *
 
 urlpatterns = [
     path('', PromoAPIList.as_view()),
+    path('header/', PromoHeaderAPIList.as_view()),
+    path('mainblock/', PromoMainBlockAPIList.as_view()),
+    path('ourservices/', PromoOurServicesAPIList.as_view()),
+    path('interview/', PromoInterviewAPIList.as_view()),
+    path('keyses/', PromoKeysesAPIList.as_view()),
+    path('feedbacks/', PromoFeedBacksAPIList.as_view()),
+    path('cooperationformats/', PromoCooperationFormatsAPIList.as_view()),
+    path('leadform/', PromoLeadFormAPIList.as_view()),
+    path('footer/', PromoFooterAPIList.as_view()),
 ]
